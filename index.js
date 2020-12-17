197eda3540d19362f1cf10865ecfab65cba1d7a5
Lloyd Montero <Lloyd.Montero@gvcgroup.com>
2020-12-17 18:37:22 +0800

    Added winston logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const winston = require('winston');
 const app = express();
 const booksRoute = require('./routes/books');
 require('dotenv').config();
@@ -10,17 +11,33 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true})); // allows array and objects
 
+// create a logger
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize({all:true})
+            )
+        }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' })
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: 'exception.log' })
+    ]
+});
+
 // routes
 app.use('/api/books', booksRoute);
 
 // connect to mongodb atlas
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}).then(() => {
-    console.log('Connected to mongodb atlas');
+    logger.info("Connected to mongoDb atlas")
 }).catch(error => {
-    console.log("There is an error " + error);
+    logger.error(error.message);
 })
 
 // start server
 app.listen(PORT, () => {
-    console.log('Server started at PORT ', PORT);
+    logger.info(`Server started at PORT ${PORT}`);
 })
--- routes/books.js
+++ routes/books.js
@@ -24,4 +24,44 @@ router.post('/', async (req, res) => {
     })
 });
 
+// GET ALL BOOKS
+router.get("/", (req, res) => {
+    Book.find()
+        .then((books) => res.send(books))
+        .catch((error) => {
+            res.status(500).send("Something went wrong");
+        });
+});
+
+// GET THE BOOK BY ID
+router.get("/:bookId", async (req, res) => {
+    const book = await Book.findById(req.params.bookId);
+    if(!book) res.status(404).send("Book not found");
+    res.send(book);
+});
+
+// UPDATE BOOK BY ID
+router.put("/:bookId", async (req, res) => {
+    const updatedBook = await Book.findOneAndUpdate(req.params.bookid, {
+        name: req.body.bookName,
+        author: {
+            name: req.body.authorName,
+            age: req.body.authorAge
+        },
+        genre: req.body.genre
+    }, {new:true, useFindAndModify: false});
+
+    if (!updatedBook) res.status(404).send("book not found");
+    res.send(updatedBook);
+});
+
+// DELETE BOOK BASED ON ID
+router.delete('/:bookId', async (req, res) => {
+    const book = await Book.findByIdAndRemove(req.params.bookId);
+    if(!book) res.status(404).send("book with id not found");
+    res.send(book);
+});
+
+
+
 module.exports = router;
